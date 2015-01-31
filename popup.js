b546d37387e3077b442938d4b7388c50a2b412d5
Kevin Xing <kxing@uwaterloo.ca>
2015-01-31 18:41:18 -0500

    popup framework for message receiving
    to change #status

--- popup.js
+++ popup.js
@@ -1,7 +1,22 @@
-window.addEventListener("gamepadconnected", function() { //connected
-	document.getElementById("status").textContent = "Gamepad is currently connected!\nUse buttons and analog sticks to control the browser.";
-});
+// wait for controls.js to sendMessage about gamepad connectedness
 
-window.addEventListener("gamepaddisconnected", function() { //disconnected
-	document.getElementById("status").textContent = "Gamepad has been disconnected,\nplug back in and open a new tab to start again.";
-});
+chrome.runtime.onMessage.addListener(
+	
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+
+    // connected state
+    if (request.gamepad == "connected"){
+      document.getElementById("status").textContent = "Gamepad is currently connected!\nUse buttons and analog sticks to control the browser.";
+      sendResponse({affirm: "message received"});
+    }
+
+    // disconnected state
+    if (request.gamepad == "disconnected"){
+      document.getElementById("status").textContent = "Gamepad has been disconnected,\nplug back in and open a new tab to start again.";
+      sendResponse({affirm: "message received"});
+    }
+
+  });
