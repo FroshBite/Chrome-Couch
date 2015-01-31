e2d81d8c3bb7d9d0464273ab41926c986944e0d4
Kevin Xing <kxing@uwaterloo.ca>
2015-01-31 04:44:50 -0500

    progress update, code needs fixing
    it's broken and i don't know why, it recognizes the controller but the
    pointer doesn't seem to be drawn on-screen AT ALL

---
+++ controls.js
@@ -0,0 +1,72 @@
+var gamepadInfo = document.getElementById("gamepad-info");
+var pointer = document.getElementById("mouse");
+var start;
+var a = 0;
+var b = 0;
+
+var rAF = window.mozRequestAnimationFrame ||
+ window.requestAnimationFrame;
+
+var rAFStop = window.mozCancelRequestAnimationFrame ||
+  window.cancelRequestAnimationFrame;
+
+window.addEventListener("gamepadconnected", function() {
+  var gp = navigator.getGamepads()[0];
+  console.log("Gamepad connected");
+  //gamepadInfo.innerHTML = "Gamepad connected at index " + gp.index + ": " + gp.id + ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.";
+
+  gameLoop();
+});
+
+window.addEventListener("gamepaddisconnected", function() {
+  //gamepadInfo.innerHTML = "Waiting for gamepad.";
+
+  rAFStop(start);
+});
+
+if(navigator.GetGamepads) {
+  // Webkit browser that uses prefixes
+  var interval = setInterval(webkitGP, 500);
+}
+
+function webkitGP() {
+  var gp = navigator.GetGamepads()[0];
+  if(gp) {
+    gamepadInfo.innerHTML = "Gamepad connected at index " + gp.index + ": " + gp.id + ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.";
+    gameLoop();
+    clearInterval(interval);
+  }
+}
+
+function gameLoop() {
+  if(navigator.GetGamepads) {
+    var gp = navigator.GetGamepads()[0];
+
+    if(gp.buttons[0] == 1) {
+      b--;
+    } else if(gp.buttons[1] == 1) {
+      a++;
+    } else if(gp.buttons[2] == 1) {
+      b++;
+    } else if(gp.buttons[3] == 1) {
+      a--;
+    }
+  } else {
+    var gp = navigator.getGamepads()[0];
+
+    if(gp.buttons[0].value > 0 || gp.buttons[0].pressed == true) {
+      b--;
+    } else if(gp.buttons[1].value > 0 || gp.buttons[1].pressed == true) {
+      a++;
+    } else if(gp.buttons[2].value > 0 || gp.buttons[2].pressed == true) {
+      b++;
+    } else if(gp.buttons[3].value > 0 || gp.buttons[3].pressed == true) {
+      a--;
+    }
+  }
+
+  pointer.style.left = a*2 + "px";
+  pointer.style.top = b*2 + "px";
+
+  var start = rAF(gameLoop);
+};
---
+++ main.js
@@ -0,0 +1,17 @@
+var mouse = document.createElement('div');
+
+mouse.id = "mouse";
+mouse.onload = function() {
+    this.parentNode.removeChild(this);
+};
+
+(document.head||document.documentElement).appendChild(mouse);
+
+console.log("Mouse created");
+
+var controls = document.createElement('script');
+controls.src = chrome.extension.getURL('controls.js');
+controls.onload = function() {
+    this.parentNode.removeChild(this);
+};
+(document.head||document.documentElement).appendChild(controls);
