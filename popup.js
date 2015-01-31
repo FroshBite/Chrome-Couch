0ff2a47dfed6e40c7a1d449b0e0cfb27d53bca3e
YaronKoller <yaronkoller@live.ca>
2015-01-31 16:39:34 -0500

    Fixed syntax error

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 window.addEventListener("gamepadconnected", function() { //connected
 	document.getElementById("status").textContent = "Gamepad is currently connected!\nUse buttons and analog sticks to control the browser.";
-}
+});
 
 window.addEventListener("gamepaddisconnected", function() { //disconnected
 	document.getElementById("status").textContent = "Gamepad has been disconnected,\nplug back in and open a new tab to start again.";
-}
+});
